74a37116f9f8585581c7d6cebae27bbed1ce3ba5
tough man <limonraycse2020@gmail.com>
2021-11-11 18:07:10 +0600

    Amit

--- lib/main.dart
+++ lib/main.dart
@@ -12,34 +12,59 @@ class _AmitState extends State<Amit> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color(0xff344955),
         appBar: AppBar(
           backgroundColor: Colors.teal,
           title: Center(
             child: Text('I AM KING'),
           ),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'I AM KING',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 45,
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  'https://images.unsplash.com/photo-1537420327992-d6e192287183?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80'),
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'I AM KING',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 45,
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                          color: Colors.purple,
+                          offset: Offset(5, 3),
+                          blurRadius: 1,
+                        )
+                      ]),
+                ),
+                Image.asset('images/Amit.png'),
+                Text(
+                  'Amit Gain',
+                  style: TextStyle(
                     color: Colors.white,
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
                     shadows: [
                       Shadow(
-                        color: Colors.purple,
-                        offset: Offset(5, 3),
-                        blurRadius: 1,
-                      )
-                    ]),
-              ),
-              Image.asset('images/Amit.png'),
-            ],
+                        color: Colors.red,
+                        offset: Offset(5, 1),
+                        blurRadius: 2,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
